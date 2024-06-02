f100710305006827c15a2cd5850c87041413f16e
chris <chriscrewbaker@gmail.com>
2024-06-02 13:52:55 -0400

    Update lorem-ipsum to reflect new progress made in app design

--- app/lorem-ipsum.R
+++ app/lorem-ipsum.R
@@ -2,10 +2,11 @@ library(here)
 library(jsonlite)
 library(glue)
 library(readr)
+library(magrittr)
 
 devtools::load_all()
 orgs <- c("GA553", "NH177", "VA321", "VA68")
-
+orgs <- c("GA553", "NH177")
 
 token <- auth_pf()
 
@@ -37,36 +38,29 @@ animals <-
     mutate(dev_null = write_local_cropped_img(id, primary_photo_cropped_large, wdir)) %>%
     ungroup()
 
-# five oldest pups on PF, fetch their rewritten bios
-
+# fetch rewritten bios
+# TODO(): refactor so i send multiple bios in one prompt
 styles <- c("interview", "pup-perspective", "sectioned")
 prompt_dfs <- sapply(styles, function(x) {
   prompt_file <- here(glue("app/prompts/{x}.json"))
   read_json(prompt_file, TRUE)
 }, simplify = FALSE, USE.NAMES = TRUE)
 
-oldest_five_bios <- animals %>% filter(is_oldest_five) %>% select(id, raw_bio)
-
 rw <- sapply(prompt_dfs, function(x) {
   parallel_request_rewrites(
-    x, oldest_five_bios$raw_bio, model = "gpt-3.5-turbo-0125"
+    x, animals$raw_bio, model = "gpt-4o"
   )
 }, simplify = FALSE, USE.NAMES = TRUE)
+rw <- as.data.frame(rw) %>%
+  rename(interview_rw = interview, pupper_rw = 2, sectioned_rw = sectioned)
 
-oldest_five_bios <- oldest_five_bios %>%
-  mutate(
-    interview_rr = rw$interview,
-    pupper_rr = rw$`pup-perspective`,
-    sectioned_rr = rw$sectioned
-  )
-
-# join back to animals and write selected cols
 out_df <- animals %>%
-  left_join(oldest_five_bios %>% select(-raw_bio)) %>%
   select(
     id, name, organization_id, url, breeds_primary, published_at, raw_bio,
-    is_oldest_five, interview_rr, pupper_rr, sectioned_rr
+    is_oldest_five
   ) %>%
-  mutate(name = gsub("[0-9]+ ", "", name))
+  mutate(name = gsub("[0-9]+ ", "", name)) %>%
+  bind_cols(as.data.frame(rw)) %>%
+  arrange(organization_id, desc(is_oldest_five))
 
 write_csv(out_df, here("app/data/lorem-ipsum-bios.csv"))
