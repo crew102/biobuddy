e4d786ac7b049541a078fe81b76a906a6e44722f
chris <chriscrewbaker@gmail.com>
2024-07-20 16:14:29 -0400

    Use a non-renv strategy for finding appropriate R library during dev and pin to specific rstudio version

--- scripts/lockfile-write.R
+++ scripts/lockfile-write.R
@@ -2,5 +2,8 @@ library(renv)
 
 snapshot(prompt = FALSE)
 lfile <- lockfile_read()
+# Needs to be 1.3-4 on mac (local) but 1.6-5 on linux (for Docker) for some
+# reason. So the offiical version is the version that works with Docker images
+# and I just have a local version that works for me on my mac.
 lfile$Packages$Matrix$Version <- "1.6-5"
 lockfile_write(lfile)
