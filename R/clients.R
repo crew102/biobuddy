d3ec277988c6e6e24bb26d40f54bfa1d8c738b55
chris baker <chriscrewbaker@gmail.com>
2024-08-28 22:15:16 -0400

    Add gmail client

--- R/clients.R
+++ R/clients.R
@@ -276,3 +276,27 @@ generic_openai_request <- function(prompt_df, model = "gpt-3.5-turbo-0125") {
   if (httr2::resp_is_error(resp)) return(httr2::resp_status_desc(resp))
   httr2::resp_body_json(resp)
 }
+
+## Gmail
+
+send_email <- function(subject, body, to = "chriscrewbaker@gmail.com") {
+  token_fi <- "gmailr-token.rds"
+  on.exit(try(unlink(token_fi)))
+  key <- "GMAILR_KEY"
+  from <- "pfanalytics787@gmail.com"
+  val <- get_secret(key)
+  Sys.setenv(key = val)
+  py$download_file_from_s3(BUCKET, token_fi, token_fi)
+  gmailr::gm_auth(token = gmailr::gm_token_read(path = token_fi, key = key))
+  print(gmailr::gm_profile())
+
+  email <-
+    gmailr::gm_mime() |>
+    gmailr::gm_to(to) |>
+    gmailr::gm_from(from) |>
+    gmailr::gm_subject(subject) |>
+    gmailr::gm_text_body(body)
+
+  gmailr::gm_send_message(email)
+}
+
