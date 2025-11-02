a97d98e4ea859964938e19e998023cf234a21120
cbaker <chriscrewbaker@gmail.com>
2025-11-02 17:31:00 -0500

    Re-org petfinder clients, also adding in ability to save results to disk for downloading complete pf db results

--- R/clients-petfinder.R
+++ R/clients-petfinder.R
@@ -1,3 +1,5 @@
+### Misc.
+
 USER_AGENT <- paste0(
   "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 ",
   "KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36"
@@ -18,26 +20,7 @@ auth_pf <- function() {
   token$access_token
 }
 
-auth_pf_headers <- function(token) {
-  add_headers(Authorization = paste("Bearer", token, sep = " "))
-}
-
-fdate <- function(x) {
-  tm <- as.POSIXlt(ymd(x), "UTC")
-  strftime(tm , "%Y-%m-%dT%H:%M:%S%z")
-}
-
-fetch_one_pf_page <- function(token, query) {
-  response <- GET(
-    "https://api.petfinder.com/v2/animals",
-    auth_pf_headers(token),
-    query = query
-  )
-  stop_for_status(response)
-  cnt <- content(response, "text", encoding = "utf-8", flatten = TRUE)
-
-  jsonlite::fromJSON(cnt)
-}
+### Animals endpoint
 
 fetch_pf_pages <- function(token,
 
@@ -70,7 +53,11 @@ fetch_pf_pages <- function(token,
 
                            size = NULL,
                            gender = NULL,
-                           age = NULL) {
+                           age = NULL,
+
+                           # Optional path to save raw httr response (base path for
+                           # multi-page requests; page number will be appended)
+                           save_raw_path = NULL) {
 
   if (length(organization) > 1) {
     organization <- paste0(organization, collapse = ",")
@@ -84,8 +71,8 @@ fetch_pf_pages <- function(token,
     location = location,
     distance = distance,
     sort = sort,
-    before_date = fdate(before_date),
-    after_date = fdate(after_date),
+    before_date = .fdate(before_date),
+    after_date = .fdate(after_date),
     limit = limit,
     size = size,
     gender = gender,
@@ -94,7 +81,24 @@ fetch_pf_pages <- function(token,
     page = 1
   )
 
-  one_res <- fetch_one_pf_page(token, query)
+  # Determine save path for first page
+  page_save_path <- if (!is.null(save_raw_path)) {
+    if (is.null(pages) || pages > 1) {
+      # Multi-page: add page number to filename
+      ext <- tools::file_ext(save_raw_path)
+      if (ext != "") {
+        paste0(tools::file_path_sans_ext(save_raw_path), "-page1.", ext)
+      } else {
+        paste0(save_raw_path, "-page1")
+      }
+    } else {
+      save_raw_path
+    }
+  } else {
+    NULL
+  }
+
+  one_res <- .fetch_one_pf_page(token, query, save_raw_path = page_save_path)
 
   if (one_res$pagination$total_count == 0) {
     return(list(
@@ -110,18 +114,29 @@ fetch_pf_pages <- function(token,
     out_animals <- list()
     suppressWarnings(out_animals[1] <- one_res)
     if (pages_res > 1) {
-      to_pull_pages <- min(pages_res, pages)
+      to_pull_pages <- if (is.null(pages)) pages_res else min(pages_res, pages)
       pb <- startpb(0, to_pull_pages)
       on.exit(closepb(pb))
       for (i in 2:to_pull_pages) {
         query$page <- i
+        # Determine save path for this page
+        page_save_path_i <- if (!is.null(save_raw_path)) {
+          ext <- tools::file_ext(save_raw_path)
+          if (ext != "") {
+            paste0(tools::file_path_sans_ext(save_raw_path), "-page", i, ".", ext)
+          } else {
+            paste0(save_raw_path, "-page", i)
+          }
+        } else {
+          NULL
+        }
         tryCatch({
-          another_res <- fetch_one_pf_page(token, query)
+          another_res <- .fetch_one_pf_page(token, query, save_raw_path = page_save_path_i)
           suppressWarnings(out_animals[i] <- another_res)
         }, error = function(e) {
           if (grepl("HTTP 500", e$message)) {
             Sys.sleep(10)
-            another_res <- fetch_one_pf_page(token, query)
+            another_res <- .fetch_one_pf_page(token, query, save_raw_path = page_save_path_i)
             suppressWarnings(out_animals[i] <- another_res)
           } else {
             stop(e)
@@ -130,45 +145,14 @@ fetch_pf_pages <- function(token,
         setpb(pb, i)
       }
     }
-    unnested <- lapply(out_animals, function(x) {
-      expected_cols <- c(
-        "id", "organization_id", "url", "type", "species", "breeds_primary",
-        "breeds_secondary", "breeds_mixed", "breeds_unknown", "colors_primary",
-        "colors_secondary", "colors_tertiary", "age", "gender", "size",
-        "coat", "attributes_spayed_neutered", "attributes_house_trained",
-        "attributes_declawed", "attributes_special_needs", "attributes_shots_current",
-        "environment_children", "environment_dogs", "environment_cats",
-        "tags", "name", "description", "organization_animal_id", "photos",
-        "primary_photo_cropped_small", "primary_photo_cropped_medium",
-        "primary_photo_cropped_large", "primary_photo_cropped_full",
-        "videos", "status", "status_changed_at", "published_at", "distance",
-        "contact_email", "contact_phone", "contact_address", "_links_self",
-        "_links_type", "_links_organization"
-      )
-
-      raw_df <- x %>%
-        unnest_wider(where(is.data.frame), names_sep = "_") %>%
-        ungroup()
-      # not super proud of this:
-      existing_cols <- colnames(raw_df)
-      for (col in expected_cols) {
-        if (!(col %in% existing_cols)) {
-          raw_df[[col]] <- NA
-        }
-      }
-      raw_df[expected_cols]
-    })
-
-    animals <- do.call(rbind, unnested)
+    animals <- .unnest_pf_animals(out_animals)
     list(
       animals = animals,
       pagination = one_res$pagination
     )
 
   } else {
-    animals <- one_res$animals %>%
-      unnest_wider(where(is.data.frame), names_sep = "_") %>%
-      ungroup()
+    animals <- .unnest_pf_animals(one_res)
     list(
       animals = animals,
       pagination = one_res$pagination
@@ -176,21 +160,92 @@ fetch_pf_pages <- function(token,
   }
 }
 
-one_orgs_request <- function(token, query) {
+.fdate <- function(x) {
+  tm <- as.POSIXlt(ymd(x), "UTC")
+  strftime(tm, "%Y-%m-%dT%H:%M:%S%z")
+}
+
+.auth_pf_headers <- function(token) {
+  add_headers(Authorization = paste("Bearer", token, sep = " "))
+}
+
+.unnest_pf_animals <- function(page_results) {
+  # page_results can be a list of page results or a single page result
+  # Each page result is a full API response with $animals component
+  if (!is.list(page_results)) {
+    page_results <- list(page_results)
+  }
+
+  # Extract animals from each page result
+  # If page_results[[1]] is an API response object with $animals component, extract it
+  # Otherwise assume page_results is already a list of animal data frames
+  if (length(page_results) > 0) {
+    first_result <- page_results[[1]]
+    # Check if first result is an API response object (has both $animals and $pagination)
+    if (is.list(first_result) && !is.data.frame(first_result) &&
+        !is.null(first_result$animals) && !is.null(first_result$pagination)) {
+      animals_list <- lapply(page_results, function(x) x$animals)
+    } else {
+      animals_list <- page_results
+    }
+  } else {
+    animals_list <- page_results
+  }
+
+  expected_cols <- c(
+    "id", "organization_id", "url", "type", "species", "breeds_primary",
+    "breeds_secondary", "breeds_mixed", "breeds_unknown", "colors_primary",
+    "colors_secondary", "colors_tertiary", "age", "gender", "size",
+    "coat", "attributes_spayed_neutered", "attributes_house_trained",
+    "attributes_declawed", "attributes_special_needs", "attributes_shots_current",
+    "environment_children", "environment_dogs", "environment_cats",
+    "tags", "name", "description", "organization_animal_id", "photos",
+    "primary_photo_cropped_small", "primary_photo_cropped_medium",
+    "primary_photo_cropped_large", "primary_photo_cropped_full",
+    "videos", "status", "status_changed_at", "published_at", "distance",
+    "contact_email", "contact_phone", "contact_address", "_links_self",
+    "_links_type", "_links_organization"
+  )
+
+  unnested <- lapply(animals_list, function(x) {
+    raw_df <- x %>%
+      unnest_wider(where(is.data.frame), names_sep = "_") %>%
+      ungroup()
+    # not super proud of this:
+    existing_cols <- colnames(raw_df)
+    for (col in expected_cols) {
+      if (!(col %in% existing_cols)) {
+        raw_df[[col]] <- NA
+      }
+    }
+    raw_df[expected_cols]
+  })
+
+  do.call(rbind, unnested)
+}
+
+.fetch_one_pf_page <- function(token, query, save_raw_path = NULL) {
   response <- GET(
-    "https://api.petfinder.com/v2/organizations",
-    auth_pf_headers(token),
+    "https://api.petfinder.com/v2/animals",
+    .auth_pf_headers(token),
     query = query
   )
   stop_for_status(response)
   cnt <- content(response, "text", encoding = "utf-8", flatten = TRUE)
+
+  if (!is.null(save_raw_path)) {
+    writeLines(cnt, save_raw_path)
+  }
+
   jsonlite::fromJSON(cnt)
 }
+
+### Organizations endpoint, getting details on all orgs on Petfinder
 
 fetch_all_orgs <- function(token) {
 
   query <- list(limit = 100, page = 1)
-  one_res <- one_orgs_request(token = token, query = query)
+  one_res <- .one_orgs_request(token = token, query = query)
 
   to_pull_pages <- one_res$pagination$total_pages
 
@@ -201,7 +256,7 @@ fetch_all_orgs <- function(token) {
   on.exit(closepb(pb))
   for (i in 2:to_pull_pages) {
     query$page <- i
-    another_res <- one_orgs_request(token, query)
+    another_res <- .one_orgs_request(token, query)
     suppressWarnings(out_animals[i] <- another_res)
     setpb(pb, i)
   }
@@ -220,23 +275,15 @@ fetch_all_orgs <- function(token) {
     rename_all(~gsub("address_|social_media_", "", .))
 }
 
-one_org_request <- function(id, token) {
+.one_orgs_request <- function(token, query) {
   response <- GET(
-    glue("https://api.petfinder.com/v2/organizations/{id}"),
-    auth_pf_headers(token)
+    "https://api.petfinder.com/v2/organizations",
+    .auth_pf_headers(token),
+    query = query
   )
   stop_for_status(response)
   cnt <- content(response, "text", encoding = "utf-8", flatten = TRUE)
-  x <- jsonlite::fromJSON(cnt)
-  as.data.frame(
-    x$organization[c("id", "name", "email", "website")],
-    stringsAsFactors = FALSE
-  )
-}
-
-fetch_some_orgs <- function(token, organizations) {
-  out <- lapply(organizations, function(x) one_org_request(x, token))
-  do.call(rbind, out)
+  jsonlite::fromJSON(cnt)
 }
 
 ## Scraping PF outside of API (needed for full bios)
